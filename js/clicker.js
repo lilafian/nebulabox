59b9da4300d14b92bc3e8ce7b1379d6bc8867b53
lilaf <lilafian74@proton.me>
2026-03-12 12:51:47 -0500

    v0.9.4 click with space + fixes

--- js/clicker.js
+++ js/clicker.js
@@ -1,7 +1,14 @@
 import { drop_box, get_inv_item } from "./inventory.js";
 import { generate_box } from "./boxutils.js";
 import { display_cursor_notification } from "./notification.js";
-import { rarity_to_string, modifier_types, box_rarities, BoxDef, ItemDef, Item } from "./items.js";
+import {
+	rarity_to_string,
+	modifier_types,
+	box_rarities,
+	BoxDef,
+	ItemDef,
+	Item,
+} from "./items.js";
 import { save_game } from "./save.js";
 
 const clicksele = document.querySelector("#clicks");
@@ -26,146 +33,168 @@ let nebulamode = false;
 let dropped_nebulamode_item = false;
 
 export function add_modifier(modifier) {
-        modifiers.push(modifier);
-        update_clicker_text()
+	modifiers.push(modifier);
+	update_clicker_text();
 }
 
 export function remove_modifier(modifier) {
-        const index = modifiers.indexOf(modifier);
-        if (index !== -1) {
-                modifiers.splice(index, 1);
-        }
-        update_clicker_text();
+	const index = modifiers.indexOf(modifier);
+	if (index !== -1) {
+		modifiers.splice(index, 1);
+	}
+	update_clicker_text();
 }
 
 export function check_nebulamode() {
-        if (nebulamode || dropped_nebulamode_item) return false;
-        const inv = get_inv_item();
-        if (inv.length < 12) return false;
-        for (const item of inv) {
-                if (item.rarity < box_rarities.ultrarare || nm_debug) {
-                        return false;
-                }
-        }
-        return true;
+	if (nebulamode || dropped_nebulamode_item) return false;
+	const inv = get_inv_item();
+	if (inv.length < 12) return false;
+	for (const item of inv) {
+		if (item.rarity < box_rarities.ultrarare || nm_debug) {
+			return false;
+		}
+	}
+	return true;
 }
 
 export function get_clicker_info() {
-        return {
-                clicks: total_clicks,
-                level: level,
-                nebulamode: nebulamode,
-                tick: current_tick,
-                clicks_left: clicks_to_next
-        };
+	return {
+		clicks: total_clicks,
+		level: level,
+		nebulamode: nebulamode,
+		tick: current_tick,
+		clicks_left: clicks_to_next,
+	};
 }
 
-export function set_clicker_info(clicks, nlevel, nnebulamode, ntick, clicks_left) {
-        total_clicks = clicks;
-        level = nlevel;
-        nebulamode = nnebulamode;
-        current_tick = ntick;
-        clicks_to_next = clicks_left;
-        update_clicker_text();
+export function set_clicker_info(
+	clicks,
+	nlevel,
+	nnebulamode,
+	ntick,
+	clicks_left,
+) {
+	total_clicks = clicks;
+	level = nlevel;
+	nebulamode = nnebulamode;
+	current_tick = ntick;
+	clicks_to_next = clicks_left;
+	update_clicker_text();
 }
 
 export function set_nebulamode(val) {
-        nebulamode = val;
-        if (nebulamode) {
-                document.body.style.background = "var(--nebulamode-gradient)";
-                document.body.style.backgroundSize = "400% 400%";
-                document.body.style.animation = "nebulamode-gradient 120s infinite";
-        }
+	nebulamode = val;
+	if (nebulamode) {
+		document.body.style.background = "var(--nebulamode-gradient)";
+		document.body.style.backgroundSize = "400% 400%";
+		document.body.style.animation =
+			"nebulamode-gradient 120s infinite";
+	}
 }
 
 export function get_nebulamode() {
-        return nebulamode;
+	return nebulamode;
 }
 
 export function next_level() {
-        level++;
+	level++;
 
-        let box = generate_box();
-        if (check_nebulamode()) {
-                dropped_nebulamode_item = true;
-                const boxdef = new BoxDef(
-                        "the nebulabox",
-                        "an intense glow radiates from this box...",
-                        [box_rarities.mythical],
-                        0,
-                        [[
-                                new ItemDef(
-                                        "universal extension",
-                                        "bends the universe to reveal rarer items. collector's favorite.",
-                                        [box_rarities.mythical],
-                                        0,
-                                        [[]]
-                                )
-                        ]]
-                )
-                box = new Item(boxdef, box_rarities.mythical);
-        }
-        drop_box(box);
+	let box = generate_box();
+	if (check_nebulamode()) {
+		dropped_nebulamode_item = true;
+		const boxdef = new BoxDef(
+			"the nebulabox",
+			"an intense glow radiates from this box...",
+			[box_rarities.mythical],
+			0,
+			[
+				[
+					new ItemDef(
+						"universal extension",
+						"bends the universe to reveal rarer items. collector's favorite.",
+						[box_rarities.mythical],
+						0,
+						[[]],
+					),
+				],
+			],
+		);
+		box = new Item(boxdef, box_rarities.mythical);
+	}
+	drop_box(box);
 
-        display_cursor_notification(`dropped ${rarity_to_string(box.rarity)} ${box.name} (now level ${level})`);
+	display_cursor_notification(
+		`dropped ${rarity_to_string(box.rarity)} ${box.name} (now level ${level})`,
+	);
 
-        clicks_to_next = Math.ceil(1 + Math.pow(level - 1, 1.5));
+	clicks_to_next = Math.ceil(1 + Math.pow(level - 1, 1.5));
 
-        update_clicker_text();
+	update_clicker_text();
 }
 
 export function update_clicker_text() {
-        levelele.innerText = `level ${level}`;
-        clicksele.innerText = `${total_clicks} clicks`;
-        clicksleftele.innerText = `${clicks_to_next} clicks until drop`;
-        modifiersele.innerText = "";
-        for (const mod of modifiers) {
-                modifiersele.innerText += `${mod.source} - ${mod.name}\n`;
-        }
+	levelele.innerText = `level ${level}`;
+	clicksele.innerText = `${total_clicks} clicks`;
+	clicksleftele.innerText = `${clicks_to_next} clicks until drop`;
+	modifiersele.innerText = "";
+	for (const mod of modifiers) {
+		modifiersele.innerText += `${mod.source} - ${mod.name}\n`;
+	}
 }
 
 export function click(from_mod = false) {
-        total_clicks++;
-        clicks_to_next--;
+	total_clicks++;
+	clicks_to_next--;
 
-        update_clicker_text();
+	update_clicker_text();
 
-        if (clicks_to_next <= 0) {
-                next_level();
-        }
+	if (clicks_to_next <= 0) {
+		next_level();
+	}
 
-        let mods = "";
-        for (const mod of modifiers) {
-                if (mod.type === modifier_types.click && from_mod === false) {
-                        mod.callback();
-                }
+	let mods = "";
+	for (const mod of modifiers) {
+		if (mod.type === modifier_types.click && from_mod === false) {
+			mod.callback();
+		}
 
-                mods += `${mod.source} - ${mod.name}\n`;
-        }
-        if (from_mod === false) display_cursor_notification(mods, 20, true);
+		mods += `${mod.source} - ${mod.name}\n`;
+	}
+	if (from_mod === false) display_cursor_notification(mods, 20, true);
 }
 
 export function tick() {
-        current_tick++;
-        for (const mod of modifiers) {
-                if (mod.type === modifier_types.tick) {
-                        mod.callback(current_tick);
-                }
-        }
+	current_tick++;
+	for (const mod of modifiers) {
+		if (mod.type === modifier_types.tick) {
+			mod.callback(current_tick);
+		}
+	}
 }
 
 export function init_click_handler() {
-        clickerele.addEventListener("click", () => {
-                for (const mod of modifiers) {
-                        if (mod.type === modifier_types.click) {
-                                mod.callback();
-                        }
-                }
+	clickerele.addEventListener("click", () => {
+		for (const mod of modifiers) {
+			if (mod.type === modifier_types.click) {
+				mod.callback();
+			}
+		}
 
-                click();
-        });
+		click();
+	});
+	document.addEventListener("keydown", (e) => {
+		if (e.key === " ") {
+			for (const mod of modifiers) {
+				if (mod.type === modifier_types.click) {
+					mod.callback();
+				}
+			}
+
+			click();
+		}
+	});
 }
 
 export function init_tick_handler() {
-        setInterval(tick, 1000 / tps);
+	setInterval(tick, 1000 / tps);
 }
--- js/notification.js
+++ js/notification.js
@@ -6,55 +6,55 @@ let mousey = 0;
 let log = [];
 
 export function init_cursor_handler() {
-        document.addEventListener("mousemove", function(event) {
-                mousex = event.clientX;
-                mousey = event.clientY;
-        });
+	document.addEventListener("mousemove", function (event) {
+		mousex = event.clientX;
+		mousey = event.clientY;
+	});
 }
 
 export function display_cursor_notification(msg, yoff = 0, nolog = false) {
-        const ele = document.createElement("span");
-        ele.className = "cursornoti";
-        ele.innerText = msg;
-        ele.style.left = `${mousex}px`;
-        ele.style.top = `${mousey + yoff}px`;
-        document.body.appendChild(ele);
-        if (!nolog) log.push(msg);
+	const ele = document.createElement("span");
+	ele.className = "cursornoti";
+	ele.innerText = msg;
+	ele.style.left = `${mousex}px`;
+	ele.style.top = `${mousey + yoff}px`;
+	document.body.appendChild(ele);
+	if (!nolog) log.push(msg);
 
-        setTimeout(() => {
-                ele.remove();
-        }, 5000);
+	setTimeout(() => {
+		ele.remove();
+	}, 5000);
 }
 
 export function display_notification_log() {
-        const overlay = get_overlay();
-        enable_overlay();
-        overlay.innerHTML = `<span>notifications</span><ul id="notis"></ul>`;
-        const ul = document.querySelector("#notis");
-        for (const noti of log) {
-                const ele = document.createElement("li");
-                ele.innerText = noti;
-                ul.appendChild(ele);
-        }
-        
+	const overlay = get_overlay();
+	enable_overlay();
+	overlay.innerHTML = `<span>notifications</span><ul id="notis"></ul>`;
+	const ul = document.querySelector("#notis");
+	for (const noti of log.toReversed()) {
+		const ele = document.createElement("li");
+		ele.innerText = noti;
+		ul.appendChild(ele);
+	}
+
 }
 
 export function display_popup_notification(msg, buttons) {
-        const ele = document.createElement("div");
-        ele.className = "popupnoti";
-        ele.innerHTML = `
+	const ele = document.createElement("div");
+	ele.className = "popupnoti";
+	ele.innerHTML = `
                         <span class="popuptext">${msg}</span>
                         <div class="popupbtnc"></div>
         `;
-        document.body.appendChild(ele);
-        for (const button of buttons) {
-                const btn = document.createElement("div");
-                btn.className = "popupbtn";
-                btn.innerText = button.msg;
-                btn.addEventListener("click", () => {
-                        button.callback();
-                        ele.remove();
-                });
-                ele.querySelector(".popupbtnc").appendChild(btn);
-        }
+	document.body.appendChild(ele);
+	for (const button of buttons) {
+		const btn = document.createElement("div");
+		btn.className = "popupbtn";
+		btn.innerText = button.msg;
+		btn.addEventListener("click", () => {
+			button.callback();
+			ele.remove();
+		});
+		ele.querySelector(".popupbtnc").appendChild(btn);
+	}
 }
